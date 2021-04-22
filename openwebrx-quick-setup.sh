896120422198ec3bd6a80146171bc7e6e192d74f
Xavier Camden <39637438+xcamden@users.noreply.github.com>
2021-04-22 11:08:59 -0400

    Update openwebrx-quick-setup.sh

--- openwebrx-quick-setup.sh
+++ openwebrx-quick-setup.sh
@@ -2,15 +2,15 @@
 sudo apt-get install build-essential git libfftw3-dev cmake libusb-1.0-0-dev
 
 #Fetch and build rtl-sdr, skip if already done (subdirectories will be created under the current directory).
-git clone git://git.osmocom.org/rtl-sdr.git
-cd rtl-sdr/
-mkdir build
-cd build
-cmake ../ -DINSTALL_UDEV_RULES=ON
-make
-sudo make install
-sudo ldconfig
-cd ../..
+#git clone git://git.osmocom.org/rtl-sdr.git
+#cd rtl-sdr/
+#mkdir build
+#cd build
+#cmake ../ -DINSTALL_UDEV_RULES=ON
+#make
+#sudo make install
+#sudo ldconfig
+#cd ../..
 
 #Disable the DVB-T driver, which would prevent the rtl_sdr tool from accessing the stick
 #(if you want to use it for DVB-T reception later, you should undo this change):
@@ -21,7 +21,7 @@ sudo update-initramfs -u #only on Ubuntu 16.04 or newer
 sudo rmmod dvb_usb_rtl28xxu 
 
 #Download OpenWebRX and libcsdr (subdirectories will be created under the current directory).
-git clone https://github.com/simonyiszk/openwebrx.git
+#git clone https://github.com/simonyiszk/openwebrx.git
 git clone https://github.com/simonyiszk/csdr.git
 
 #Compile libcsdr (which is a dependency of OpenWebRX)
@@ -30,8 +30,8 @@ make
 sudo make install
 
 #Edit OpenWebRX config or leave defaults
-nano ../openwebrx/config_webrx.py 
+#nano ../openwebrx/config_webrx.py 
 
 #Run OpenWebRX
-cd ../openwebrx
-./openwebrx.py
+#cd ../openwebrx
+#./openwebrx.py
